7f23705e119f4755c5e9a25295000997089f44c2
Osmane Kalache <osminosm@gmail.com>
2023-04-02 00:20:38 +0100

    main program to run example (working on real leds)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use rs_ws281x::ControllerBuilder;
+use rs_ws281x::ChannelBuilder;
+use rs_ws281x::StripType;
+
 fn main() {
-    println!("Hello, world!");
+    // Construct a single channel controller. Note that the
+    // Controller is initialized by default and is cleaned up on drop
+
+    let mut controller = ControllerBuilder::new()
+        .freq(800_000)
+        .dma(10)
+        .channel(
+            0, // Channel Index
+            ChannelBuilder::new()
+                .pin(18) // GPIO 10 = SPI0 MOSI
+                .count(60*6) // Number of LEDs
+                .strip_type(StripType::Ws2811Rgb)
+                .brightness(20) // default: 255
+                .build(),
+        )
+        .build()
+        .unwrap();
+
+    let leds = controller.leds_mut(0);
+
+    for led in leds {
+        *led = [0, 0, 255, 0];
+    }
+
+    controller.render().unwrap();
 }
